eb1b300a97aa19dcfb868b78725102d0769597a1
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-05-10 19:57:24 -0300

    fix

--- compiler/index.ts
+++ compiler/index.ts
@@ -11,12 +11,20 @@ const logAll = () => {
     const type = stats.isDirectory() ? '📁' : '-'
     console.log(`${type} ${file}`)
   })
+}
 
+const createDir = (dir: string) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true })
+    console.log(`Created directory: ${dir}`)
+  }
 }
 
 function copyIndexFile() {
 
   logAll()
+
+  createDir('api')
 
   const sourcePath = path.join(dirName, 'src/index.ts')
   const targetPath = path.join(dirName, 'api/index.ts')
